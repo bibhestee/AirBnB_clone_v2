4d8269f78d0bb2144d17126ce0c25e41a02202db
Bibest <bibhestee@gmail.com>
2022-11-11 15:39:04 +0000

    Install a flask app

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+
+    < 0-HELLO_ROUTE >
+
+    This script starts a Flask web application that listen on 0.0.0.0 port 5000
+    When query with home '/' should display "Hello HBNB!"
+    Also, the option strict_slashes=False is used in route definition.
+
+"""
+
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    """
+        < Hello >
+
+    This function returns the string "Hello HBNB!" when '/' is requested.
+
+    """
+    return "Hello HBNB!"
+
+
+# Run the flask app on all addresses
+app.run(host='0.0.0.0')
