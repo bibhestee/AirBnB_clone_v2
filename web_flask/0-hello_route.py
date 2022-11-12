42e6f401746940f37a50ad059e71a2b0bf255bea
Bibest <bibhestee@gmail.com>
2022-11-12 20:57:19 +0000

    hello route: reduce content size

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,12 +1,8 @@
 #!/usr/bin/python3
-"""
-
-    < 0-HELLO_ROUTE >
-
+""" < 0-HELLO_ROUTE >
     This script starts a Flask web application that listen on 0.0.0.0 port 5000
     When query with home '/' should display "Hello HBNB!"
     Also, the option strict_slashes=False is used in route definition.
-
 """
 
 from flask import Flask
@@ -15,12 +11,8 @@ app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
 def hello():
-    """
-        < Hello >
-
-    This function returns the string "Hello HBNB!" when '/' is requested.
-
-    """
+    """ < Hello >
+    This function returns the string "Hello HBNB!" when '/' is requested. """
     return "Hello HBNB!"
 
 
