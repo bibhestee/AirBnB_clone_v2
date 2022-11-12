6a16b7041ae1a1430a463dbe8df057208813c0e4
Bibest <bibhestee@gmail.com>
2022-11-12 20:25:46 +0000

    Hbnb route

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""
+
+    < 1-HBNB_ROUTE >
+
+    This script starts a Flask web application that listen on 0.0.0.0 port 5000
+    When query with home '/hbnb' should display "HBNB!"
+    Also, the option strict_slashes=False is used in route definition.
+
+"""
+
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    """
+        < Hello >
+
+    This function returns the string "Hello HBNB!" when '/' is requested.
+
+    """
+    return "Hello HBNB!"
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb_route():
+    """
+        < HBNB >
+
+    This function returns the string "HBNB" when '/hbnb' is requested.
+
+    """
+    return "HBNB"
+
+
+# Run the flask app on all addresses
+app.run(host='0.0.0.0')
