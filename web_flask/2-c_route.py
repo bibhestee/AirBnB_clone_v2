10be49fbbd383367ccb2fec4c5d2371acf203edd
Bibest <bibhestee@gmail.com>
2022-11-13 13:45:47 +0000

    C route: display C <text> when /c/<text> is queried.

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python3
+"""
+
+    < 2-C_ROUTE >
+
+    This script starts a Flask web application that listen on 0.0.0.0 port 5000
+    - When query with home '/hbnb' should display "HBNB!"
+    - When query with '/c/<text>' should display "C <text>"
+    - Also, the option strict_slashes=False is used in route definition.
+
+"""
+
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    """
+        < Hello >
+
+    This function returns the string "Hello HBNB!" when '/' is requested.
+
+    """
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb_route():
+    """
+        < HBNB >
+
+    This function returns the string "HBNB" when '/hbnb' is requested.
+
+    """
+    return "HBNB"
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_route(text):
+    """
+        < C Route >
+
+    This function returns the string "C <text>" when '/c/<text>' is requested.
+
+    """
+    text = text.replace('_', ' ')
+    return f"C {text}"
+
+
+# Run the flask app on all addresses
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')
